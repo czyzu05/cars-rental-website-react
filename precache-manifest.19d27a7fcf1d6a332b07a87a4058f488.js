467bda0a4041523c7348d47a11a520de0399df6e
czyzu05 <damian.czyz1998@gmail.com>
2020-11-17 15:44:55 +0100

    Updates

---
+++ precache-manifest.19d27a7fcf1d6a332b07a87a4058f488.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dcf7f3bc3562d6456952e45f8c85196",
+    "url": "/cars-rental-website-react/index.html"
+  },
+  {
+    "revision": "0f5cfb29a5e7626d98bd",
+    "url": "/cars-rental-website-react/static/css/main.0f87f420.chunk.css"
+  },
+  {
+    "revision": "83d08a842ff521b71cca",
+    "url": "/cars-rental-website-react/static/js/2.1da6bb78.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cars-rental-website-react/static/js/2.1da6bb78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f5cfb29a5e7626d98bd",
+    "url": "/cars-rental-website-react/static/js/main.c4d01679.chunk.js"
+  },
+  {
+    "revision": "f77f50bc961a7b652fed",
+    "url": "/cars-rental-website-react/static/js/runtime-main.c8dab87d.js"
+  },
+  {
+    "revision": "a12d4882e3cd24c94346a00bf7792415",
+    "url": "/cars-rental-website-react/static/media/img-1.a12d4882.jpg"
+  },
+  {
+    "revision": "674b8816a341b57fa078da3696c95a34",
+    "url": "/cars-rental-website-react/static/media/img-2.674b8816.jpg"
+  },
+  {
+    "revision": "637d8d1ca2a20303129041b0384fa23b",
+    "url": "/cars-rental-website-react/static/media/img-3.637d8d1c.jpg"
+  },
+  {
+    "revision": "449bb1e71404b8cc0d5a5ecea6a71a60",
+    "url": "/cars-rental-website-react/static/media/img-4.449bb1e7.jpg"
+  },
+  {
+    "revision": "ce053346effccf8e85e720dc43d18259",
+    "url": "/cars-rental-website-react/static/media/img-5.ce053346.jpg"
+  },
+  {
+    "revision": "1fb7423103b84d5171482a29f170b65d",
+    "url": "/cars-rental-website-react/static/media/img-home.1fb74231.jpg"
+  }
+]);
